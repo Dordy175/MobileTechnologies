0bde5a15410f39e89f38be68c4c6021f946d885b
Dordy175 <ladislavdorotik@gmail.com>
2020-12-02 21:02:08 +0100

    Room - added newWorldActivity

--- dordy-room/app/src/main/java/cz/dordy/mt/room/wordsample/Word.kt
+++ dordy-room/app/src/main/java/cz/dordy/mt/room/wordsample/Word.kt
@@ -10,10 +10,4 @@ import androidx.room.PrimaryKey
 
 //@Entity(tableName = "word_table") Each @Entity class represents a SQLite table. Annotate your class declaration to indicate that it's an entity. You can specify the name of the table if you want it to be different from the name of the class. This names the table "word_table".
 @Entity(tableName = "word_table")
-class Word(
-
-    @PrimaryKey(autoGenerate = true) val id: Int,
-
-    @ColumnInfo(name = "word") val word: String
-
-)
+data class Word(@PrimaryKey @ColumnInfo(name = "word") val word: String)
