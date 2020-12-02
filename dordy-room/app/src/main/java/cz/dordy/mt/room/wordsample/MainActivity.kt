815acbd77f1a091460abf8260ea698fb8fe83a3f
Dordy175 <ladislavdorotik@gmail.com>
2020-12-02 17:21:33 +0100

    Room Initial Commit
    Created new app (empty activity)
    Created and compiled..

---
+++ dordy-room/app/src/main/java/cz/dordy/mt/room/wordsample/MainActivity.kt
@@ -0,0 +1,11 @@
+package cz.dordy.mt.room.wordsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
